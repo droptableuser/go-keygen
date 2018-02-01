2fb2dccce2a754ccaea1d29c402065f6ee25c0e0
Markus <markus@droptableuser.me>
2018-02-01 18:53:30 +0100

    added a bit of documentation

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 )
@@ -11,34 +12,43 @@ const (
 	printable  = 0x21
 	multiplier = 0x5d
 	filler     = 0x41
-	usage      = "Usage:\n go-keygen login\nCalculates a password to the given login name for the passcheck application"
+	usage      = "Usage:\n go-keygen -login=NAME\nCalculates a password to the given login name for the passcheck application"
 )
 
-var password string
-
 func main() {
-	arguments := os.Args[1:]
-	if len(arguments) != 1 {
-		fmt.Println("Please supply only a login value")
-		os.Exit(1)
-	}
-	login := arguments[0]
-	if login == "-h" || login == "--help" {
+	// stores the generated password
+	var password string
+
+	helpPtr := flag.Bool("help", false, "display help")
+	hPtr := flag.Bool("h", false, "display help")
+	var login string
+	flag.StringVar(&login, "login", "", "the login name")
+	flag.Parse()
+	if *hPtr || *helpPtr {
 		fmt.Println(usage)
 		os.Exit(0)
 	}
+	if login == "" {
+		fmt.Println(usage)
+		os.Exit(1)
+	}
+
 	counter := 0
+	// at most 8 password characters are generated that way
 	for counter < 8 && counter < len(login) {
 		if counter == 0 {
+			// for the first password character use the first two characters of the username
 			x := keygen(int(login[counter]), int(login[counter+1]))
 			password += string(x)
 			counter++
 		} else {
+			// all other characters are genereated by using the last character from the password, and a character from the login
 			x := keygen((int(login[counter]) << 2), int(password[counter-1]))
 			password += string(x)
 			counter++
 		}
 	}
+	// the remaining password characters are generated up to the final length of 10
 	for 10-counter > 0 {
 		x := filler + counter
 		y := int(password[counter-1])
@@ -50,6 +60,8 @@ func main() {
 	fmt.Println(password)
 
 }
+
+// derived function from the passcheck application
 func keygen(l int, r int) (sym int) {
 	x := l + r
 	sym = printable + x - ((x*expansion)>>shift)*multiplier
